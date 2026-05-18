ac57ec6313961a17c479f95bcceb69b73b1e529c
Steven Allen <steven@stebalien.com>
2026-05-18 12:28:30 -0700

    Fix recursion in custom definitions

--- elfeed-org.el
+++ elfeed-org.el
@@ -44,9 +44,38 @@
 (require 'elfeed-db)
 (require 'org-element)
 
-(defvar elfeed-org-files)
-(defvar elfeed-org-include-tag)
-(defvar elfeed-org-exclude-tag)
+(defgroup elfeed-org nil
+  "Org-mode integration for Elfeed."
+  :group 'elfeed)
+
+(defun elfeed-org--set-and-update (symbol value)
+  (set-default-toplevel-value symbol value)
+  (when (featurep 'elfeed-org)
+    (elfeed-org-update)))
+
+(defcustom elfeed-org-files nil
+  "List of `org-mode' files to search for feeds.
+
+Each file should contain headlines tagged with
+`elfeed-org-include-tag', either directly or via tag inheritance.
+The link text of each such headline is treated as a feed URL. See
+the file commentary for a complete usage example."
+  :type '(repeat file)
+  :set #'elfeed-org--set-and-update
+  :group 'elfeed-org)
+
+(defcustom elfeed-org-include-tag "elfeed"
+  "Tag used to identify Elfeed feeds."
+  :type '(repeat string)
+  :set #'elfeed-org--set-and-update
+  :group 'elfeed-org)
+
+(defcustom elfeed-org-exclude-tag "ignore"
+  "Tag used to ignore Elfeed feeds."
+  :type '(repeat string)
+  :set #'elfeed-org--set-and-update
+  :group 'elfeed-org)
+
 
 (defun elfeed-org--headline-link (headline)
   "Extract the first link from HEADLINE's title.
@@ -121,38 +150,8 @@ saving an `org-mode' file listed in `elfeed-org-files' automatically
 refreshes the feed list."
   (add-hook 'after-save-hook 'elfeed-org-update nil t))
 
-(defgroup elfeed-org nil
-  "Org-mode integration for Elfeed."
-  :group 'elfeed)
-
-(defun elfeed-org--set-and-update (symbol value)
-  (set-default-toplevel-value symbol value)
-  (elfeed-org-update))
-
-(defcustom elfeed-org-files nil
-  "List of `org-mode' files to search for feeds.
-
-Each file should contain headlines tagged with
-`elfeed-org-include-tag', either directly or via tag inheritance.
-The link text of each such headline is treated as a feed URL. See
-the file commentary for a complete usage example."
-  :type '(repeat file)
-  :set #'elfeed-org--set-and-update
-  :group 'elfeed-org)
-
-(defcustom elfeed-org-include-tag "elfeed"
-  "Tag used to identify Elfeed feeds."
-  :type '(repeat string)
-  :set #'elfeed-org--set-and-update
-  :group 'elfeed-org)
-
-(defcustom elfeed-org-exclude-tag "ignore"
-  "Tag used to ignore Elfeed feeds."
-  :type '(repeat string)
-  :set #'elfeed-org--set-and-update
-  :group 'elfeed-org)
-
 (add-hook 'org-mode-hook #'elfeed-org--org-setup)
+(elfeed-org-update)
 
 (provide 'elfeed-org)
 ;;; elfeed-org.el ends here
