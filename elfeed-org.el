70ccd2cc5d9713da3e44299487b66bff5a764a5e
Steven Allen <steven@stebalien.com>
2026-05-20 13:10:25 -0700

    Specify that elfeed-org is not a part of Emacs

--- elfeed-org.el
+++ elfeed-org.el
@@ -8,6 +8,8 @@
 ;; Package-Requires: ((emacs "27.1") (elfeed "3.4.2") (org "9.7"))
 ;; URL: https://github.com/Stebalien/elfeed-org
 
+;; This file is NOT a part of GNU Emacs.
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
